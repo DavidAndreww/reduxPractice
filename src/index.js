d21b5bb0c98482097cd1b074e35b310c0f9b78b4
David Bille <d_bille@yahoo.com>
2020-02-22 15:51:16 -0600

    ready to implement redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux'
-import store from './redux/store'
 
-render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+render(<App />, document.getElementById('root'));
